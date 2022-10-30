d4963aacc552c6c45c867e90468ffff8b834c60f
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2022-10-30 23:37:37 +0600

    oouhulou

--- Practice/quick_sort.py
+++ Practice/quick_sort.py
@@ -11,11 +11,13 @@ def partition(L, low, high):
             L[j], L[i] = L[i], L[j]
 
     L[i+1], L[high] = L[high], L[i+1]
+
     return i+1
 
 
 def quick_sort(L, low, high):
 
+    print(L[low:high])
     if low>=high:
         return 
 
@@ -25,7 +27,7 @@ def quick_sort(L, low, high):
 
 
 if __name__ == "__main__":
-    A = [2,1,1,0,3]
+    A = [2,1,1,0,1,1,9,8]
 
     quick_sort(A,0, len(A)-1)
     print(A)
